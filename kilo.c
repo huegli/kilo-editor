9edb683d728b1259f1b70944c4270a45264c6e83
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2023-01-16 23:27:11 +0000

    Chapter 3 - window side hard

--- kilo.c
+++ kilo.c
@@ -53,7 +53,7 @@ void enableRawMode() {
 
 }
 
-char editorReadyKey() {
+char editorReadKey() {
     int nread;
     char c;
     while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
@@ -62,11 +62,30 @@ char editorReadyKey() {
     return c;
 }
 
+int getCursorPosition(int *rows, int *cols) {
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+    printf("\r\n");
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1) {
+        if (iscntrl(c)) {
+            printf("%d\r\n", c);
+        } else {
+            printf("%d ('%c')\r\n", c, c);
+        }
+    }
+
+    editorReadKey();
+
+    return -1;
+}
+
 int getWindowSize(int *rows, int *cols) {
     struct winsize ws;
 
-    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-        return -1;
+    if (1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)  return -1;
+        return getCursorPosition(rows, cols);
     } else {
         *cols = ws.ws_col;
         *rows = ws.ws_row;
@@ -94,7 +113,7 @@ void editorRefreshScreen() {
 
 /*** input ***/
 void editorProcessKeypress() {
-    char c = editorReadyKey();
+    char c = editorReadKey();
 
     switch (c) {
         case CTRL_KEY('q'):
