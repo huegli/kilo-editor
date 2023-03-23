89bdcf3299a248470bd761ba8e5ff8433ef15d73
Nikolai Schlegel <nschlegel@apple.com>
2023-03-23 14:29:29 -0700

    Data structures and terminal function removal

--- kilo-cppt/main.cpp
+++ kilo-cppt/main.cpp
@@ -13,246 +13,83 @@
 
 // /*** defines ***/
 
-// #define KILO_VERSION "0.0.1"
-// #define KILO_TAB_STOP 8
-// #define KILO_QUIT_TIMES 3
+const std::string KILO_VERSION{"0.0.1"};
+const int KILO_TAB_STOP{8};
+const int KILO_QUIT_TIMES{3};
 
-// #define CTRL_KEY(k) ((k)&0x1f)
+enum editorHighlight {
+  HL_NORMAL = 0,
+  HL_COMMENT,
+  HL_MLCOMMENT,
+  HL_KEYWORD1,
+  HL_KEYWORD2,
+  HL_STRING,
+  HL_NUMBER,
+  HL_MATCH
+};
 
-// enum editorKey {
-//   BACKSPACE = 127,
-//   ARROW_LEFT = 1000,
-//   ARROW_RIGHT,
-//   ARROW_UP,
-//   ARROW_DOWN,
-//   DEL_KEY,
-//   HOME_KEY,
-//   END_KEY,
-//   PAGE_UP,
-//   PAGE_DOWN
-// };
+#define HL_HIGHLIGHT_NUMBERS (1 << 0)
+#define HL_HIGHLIGHT_STRINGS (1 << 1)
 
-// enum editorHighlight {
-//   HL_NORMAL = 0,
-//   HL_COMMENT,
-//   HL_MLCOMMENT,
-//   HL_KEYWORD1,
-//   HL_KEYWORD2,
-//   HL_STRING,
-//   HL_NUMBER,
-//   HL_MATCH
-// };
+/*** data ***/
 
-// #define HL_HIGHLIGHT_NUMBERS (1<<0)
-// #define HL_HIGHLIGHT_STRINGS (1<<1)
+struct editorSyntax {
+  const char *filetype;
+  const char **filematch;
+  const char **keywords;
+  const char *singleline_comment_start;
+  const char *multiline_comment_start;
+  const char *multiline_comment_end;
+  int flags;
+};
 
-// /*** data ***/
+typedef struct erow {
+  int idx;
+  int size;
+  int rsize;
+  char *chars;
+  char *render;
+  unsigned char *hl;
+  int hl_open_comment;
+} erow;
 
-// struct editorSyntax {
-//   char *filetype;
-//   char **filematch;
-//   char **keywords;
-//   char *singleline_comment_start;
-//   char *multiline_comment_start;
-//   char *multiline_comment_end;
-//   int flags;
-// };
+struct editorConfig {
+  int cx, cy;
+  int rx;
+  int rowoff;
+  int coloff;
+  int screenrows;
+  int screencols;
+  int numrows;
+  erow *row;
+  int dirty;
+  char *filename;
+  char statusmsg[80];
+  time_t statusmsg_time;
+  struct editorSyntax *syntax;
+};
 
-// typedef struct erow {
-//   int idx;
-//   int size;
-//   int rsize;
-//   char *chars;
-//   char *render;
-//   unsigned char *hl;
-//   int hl_open_comment;
-// } erow;
-
-// struct editorConfig {
-//   int cx, cy;
-//   int rx;
-//   int rowoff;
-//   int coloff;
-//   int screenrows;
-//   int screencols;
-//   int numrows;
-//   erow *row;
-//   int dirty;
-//   char *filename;
-//   char statusmsg[80];
-//   time_t statusmsg_time;
-//   struct editorSyntax *syntax;
-//   struct termios orig_termios;
-// };
-
-// struct editorConfig E;
+struct editorConfig E;
 
 // /*** filetypes ***/
 
-// char *C_HL_extensions[] = { ".c", ".h", ".cpp", NULL };
-// char *C_HL_keywords[] = {
-//   "switch", "if", "while", "for", "break", "continue", "return", "else",
-//   "struct", "union", "typedef", "static", "enum", "class", "case",
-//   "int|", "long|", "double|", "float|", "char|", "unsigned|", "signed|",
-//   "void|", NULL
-// };
+const char *C_HL_extensions[] = {".c", ".h", ".cpp", nullptr};
+const char *C_HL_keywords[] = {
+    "switch", "if",    "while",     "for",     "break",   "continue",
+    "return", "else",  "struct",    "union",   "typedef", "static",
+    "enum",   "class", "case",      "int|",    "long|",   "double|",
+    "float|", "char|", "unsigned|", "signed|", "void|",   nullptr};
 
-// struct editorSyntax HLDB[] = {
-//   {
-//     "c",
-//     C_HL_extensions,
-//     C_HL_keywords,
-//     "//", "/*", "*/",
-//     HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS
-//   },
-// };
+struct editorSyntax HLDB[] = {
+    {"c", C_HL_extensions, C_HL_keywords, "//", "/*", "*/",
+     HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS},
+};
 
-// #define HLDB_ENTRIES (sizeof(HLDB) / sizeof(HLDB[0]))
+#define HLDB_ENTRIES (sizeof(HLDB) / sizeof(HLDB[0]))
 
 // /*** prototypes ***/
 
-// void editorSetStatusMessage(const char *fmt, ...);
-// void editorRefreshScreen();
-// char *editorPrompt(char *prompt, void (*callback)(char *, int));
-
-// /*** terminal ***/
-
-// void die(const char *s) {
-//   write(STDOUT_FILENO, "\x1b[2J", 4);
-//   write(STDOUT_FILENO, "\x1b[H", 3);
-
-//   perror(s);
-//   exit(1);
-// }
-
-// void disableRawMode() {
-//   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
-//     die("tcsetattr");
-// }
-
-// void enableRawMode() {
-//   if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1)
-//     die("tcgetattr");
-//   atexit(disableRawMode);
-
-//   struct termios raw = E.orig_termios;
-//   raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
-//   raw.c_oflag &= ~(OPOST);
-//   raw.c_cflag |= ~(CS8);
-//   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
-//   raw.c_cc[VMIN] = 0;
-//   raw.c_cc[VTIME] = 1;
-
-//   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
-//     die("tcsetattr");
-// }
-
-// int editorReadKey() {
-//   int nread;
-//   char c;
-//   while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
-//     if (nread == -1 && errno != EAGAIN)
-//       die("read");
-//   }
-
-//   if (c == '\x1b') {
-//     char seq[3];
-
-//     if (read(STDIN_FILENO, &seq[0], 1) != 1)
-//       return '\x1b';
-//     if (read(STDIN_FILENO, &seq[1], 1) != 1)
-//       return '\x1b';
-
-//     if (seq[0] == '[') {
-//       if (seq[1] >= '0' && seq[1] <= '9') {
-//         if (read(STDIN_FILENO, &seq[2], 1) != 1)
-//           return '\x1b';
-//         if (seq[2] == '~') {
-//           switch (seq[1]) {
-//           case '1':
-//             return HOME_KEY;
-//           case '3':
-//             return DEL_KEY;
-//           case '4':
-//             return END_KEY;
-//           case '5':
-//             return PAGE_UP;
-//           case '6':
-//             return PAGE_DOWN;
-//           case '7':
-//             return HOME_KEY;
-//           case '8':
-//             return END_KEY;
-//           }
-//         }
-//       } else {
-//         switch (seq[1]) {
-//         case 'A':
-//           return ARROW_UP;
-//         case 'B':
-//           return ARROW_DOWN;
-//         case 'C':
-//           return ARROW_RIGHT;
-//         case 'D':
-//           return ARROW_LEFT;
-//         case 'H':
-//           return HOME_KEY;
-//         case 'F':
-//           return END_KEY;
-//         }
-//       }
-//     } else if (seq[0] == '0') {
-//       switch (seq[1]) {
-//       case 'H':
-//         return HOME_KEY;
-//       case 'F':
-//         return END_KEY;
-//       }
-//     }
-
-//     return '\x1b';
-//   } else {
-//     return c;
-//   }
-// }
-
-// int getCursorPosition(int *rows, int *cols) {
-//   char buf[32];
-//   unsigned int i = 0;
-
-//   if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4)
-//     return -1;
-
-//   while (i < sizeof(buf) - 1) {
-//     if (read(STDIN_FILENO, &buf[i], 1) != 1)
-//       break;
-//     if (buf[i] == 'R')
-//       break;
-//     i++;
-//   }
-//   buf[i] = '\0';
-
-//   if (buf[0] != '\x1b' || buf[1] != '[')
-//     return -1;
-//   if (sscanf(&buf[2], "%d;%d", rows, cols) != 2)
-//     return -1;
-
-//   return 0;
-// }
-
-// int getWindowSize(int *rows, int *cols) {
-//   struct winsize ws;
-
-//   if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-//     if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
-//       return -1;
-//     return getCursorPosition(rows, cols);
-//   } else {
-//     *cols = ws.ws_col;
-//     *rows = ws.ws_row;
-//     return 0;
-//   }
-// }
+char *editorPrompt(const char *prompt, void (*callback)(char *, int));
 
 // /*** syntax highlighting ***/
 
