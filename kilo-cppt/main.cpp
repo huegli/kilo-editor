c3d2aefe64fb4568a46e99957ec7caf9023ccc65
Nikolai Schlegel <nschlegel@apple.com>
2023-03-24 10:08:43 -0700

    Finished row operations

--- kilo-cppt/main.cpp
+++ kilo-cppt/main.cpp
@@ -263,124 +263,128 @@ void editorSelectSyntaxHighlight() {
   }
 }
 
-// /*** row operations ***/
+/*** row operations ***/
 
-// int editorRowCxToRx(erow *row, int cx) {
-//   int rx = 0;
-//   int j;
-//   for (j = 0; j < cx; j++) {
-//     if (row->chars[j] == '\t')
-//       rx += (KILO_TAB_STOP - 1) - (rx % KILO_TAB_STOP);
-//     rx++;
-//   }
-//   return rx;
-// }
+int editorRowCxToRx(erow *row, int cx) {
+  int rx = 0;
+  for (int j = 0; j < cx; j++) {
+    if (row->chars[j] == '\t')
+      rx += (KILO_TAB_STOP - 1) - (rx % KILO_TAB_STOP);
+    rx++;
+  }
+  return rx;
+}
 
-// int editorRowRxToCx(erow *row, int rx) {
-//   int cur_rx = 0;
-//   int cx;
-//   for (cx = 0; cx < row->size; cx++) {
-//     if (row->chars[cx] == '\t')
-//       cur_rx += (KILO_TAB_STOP - 1) - (cur_rx % KILO_TAB_STOP);
-//     cur_rx++;
+int editorRowRxToCx(erow *row, int rx) {
+  int cur_rx = 0;
+  int cx{};
+  for (cx = 0; cx < row->size; cx++) {
+    if (row->chars[cx] == '\t')
+      cur_rx += (KILO_TAB_STOP - 1) - (cur_rx % KILO_TAB_STOP);
+    cur_rx++;
 
-//     if (cur_rx > rx) return cx;
-//   }
-//   return cx;
-// }
+    if (cur_rx > rx)
+      return cx;
+  }
+  return cx;
+}
 
-// void editorUpdateRow(erow *row) {
-//   int tabs = 0;
-//   int j;
-//   for (j = 0; j < row->size; j++)
-//     if (row->chars[j] == '\t')
-//       tabs++;
+void editorUpdateRow(erow *row) {
+  int tabs = 0;
+  for (int j = 0; j < row->size; j++)
+    if (row->chars[j] == '\t')
+      tabs++;
 
-//   free(row->render);
-//   row->render = malloc(row->size + tabs * (KILO_TAB_STOP - 1) + 1);
+  free(row->render);
+  row->render = (char *)malloc(row->size + tabs * (KILO_TAB_STOP - 1) + 1);
 
-//   int idx = 0;
-//   for (j = 0; j < row->size; j++) {
-//     if (row->chars[j] == '\t') {
-//       row->render[idx++] = ' ';
-//       while (idx % KILO_TAB_STOP != 0)
-//         row->render[idx++] = ' ';
-//     } else {
-//       row->render[idx++] = row->chars[j];
-//     }
-//   }
-//   row->render[idx] = '\0';
-//   row->rsize = idx;
+  int idx = 0;
+  for (int j = 0; j < row->size; j++) {
+    if (row->chars[j] == '\t') {
+      row->render[idx++] = ' ';
+      while (idx % KILO_TAB_STOP != 0)
+        row->render[idx++] = ' ';
+    } else {
+      row->render[idx++] = row->chars[j];
+    }
+  }
+  row->render[idx] = '\0';
+  row->rsize = idx;
 
-//   editorUpdateSyntax(row);
-// }
+  editorUpdateSyntax(row);
+}
 
-// void editorInsertRow(int at, char *s, size_t len) {
-//   if (at < 0 ||  at > E.numrows) return;
+void editorInsertRow(int at, const char *s, std::size_t len) {
+  if (at < 0 || at > E.numrows)
+    return;
 
-//   E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
-//   memmove(&E.row[at + 1], &E.row[at], sizeof(erow) * (E.numrows - at));
-//   for (int j = at + 1; j <= E.numrows; j++) E.row[j].idx++;
+  E.row = (erow *)realloc(E.row, sizeof(erow) * (E.numrows + 1));
+  memmove(&E.row[at + 1], &E.row[at], sizeof(erow) * (E.numrows - at));
+  for (int j = at + 1; j <= E.numrows; j++)
+    E.row[j].idx++;
 
-//   E.row[at].idx = at;
+  E.row[at].idx = at;
 
-//   E.row[at].size = len;
-//   E.row[at].chars = malloc(len + 1);
-//   memcpy(E.row[at].chars, s, len);
-//   E.row[at].chars[len] = '\0';
+  E.row[at].size = len;
+  E.row[at].chars = (char *)malloc(len + 1);
+  memcpy(E.row[at].chars, s, len);
+  E.row[at].chars[len] = '\0';
 
-//   E.row[at].rsize = 0;
-//   E.row[at].render = NULL;
-//   E.row[at].hl = NULL;
-//   E.row[at].hl_open_comment = 0;
-//   editorUpdateRow(&E.row[at]);
+  E.row[at].rsize = 0;
+  E.row[at].render = nullptr;
+  E.row[at].hl = nullptr;
+  E.row[at].hl_open_comment = 0;
+  editorUpdateRow(&E.row[at]);
 
-//   E.numrows++;
-//   E.dirty++;
-// }
+  E.numrows++;
+  E.dirty++;
+}
 
-// void editorFreeRow(erow *row) {
-//   free(row->render);
-//   free(row->chars);
-//   free(row->hl);
-// }
+void editorFreeRow(erow *row) {
+  free(row->render);
+  free(row->chars);
+  free(row->hl);
+}
 
-// void editorDelRow(int at) {
-//   if (at < 0 || at >= E.numrows) return;
-//   editorFreeRow(&E.row[at]);
-//   memmove(&E.row[at], &E.row[at + 1], sizeof(erow) * (E.numrows - at -1));
-//   for (int j = at; j < E.numrows - 1; j++) E.row[j].idx--;
-//   E.numrows--;
-//   E.dirty++;
-// }
+void editorDelRow(int at) {
+  if (at < 0 || at >= E.numrows)
+    return;
+  editorFreeRow(&E.row[at]);
+  memmove(&E.row[at], &E.row[at + 1], sizeof(erow) * (E.numrows - at - 1));
+  for (int j = at; j < E.numrows - 1; j++)
+    E.row[j].idx--;
+  E.numrows--;
+  E.dirty++;
+}
 
-// void editorRowInsertChar(erow *row, int at, int c) {
-//   if (at < 0 || at > row->size)
-//     at = row->size;
-//   row->chars = realloc(row->chars, row->size + 2);
-//   memmove(&row->chars[at + 1], &row->chars[at], row->size - at + 1);
-//   row->size++;
-//   row->chars[at] = c;
-//   editorUpdateRow(row);
-//   E.dirty++;
-// }
+void editorRowInsertChar(erow *row, int at, int c) {
+  if (at < 0 || at > row->size)
+    at = row->size;
+  row->chars = (char *)realloc(row->chars, row->size + 2);
+  memmove(&row->chars[at + 1], &row->chars[at], row->size - at + 1);
+  row->size++;
+  row->chars[at] = c;
+  editorUpdateRow(row);
+  E.dirty++;
+}
 
-// void editorRowAppendString(erow *row, char *s, size_t len) {
-//   row->chars = realloc(row->chars, row->size + len + 1);
-//   memcpy(&row->chars[row->size], s, len);
-//   row->size += len;
-//   row->chars[row->size] = '\0';
-//   editorUpdateRow(row);
-//   E.dirty++;
-// }
+void editorRowAppendString(erow *row, char *s, size_t len) {
+  row->chars = (char *)realloc(row->chars, row->size + len + 1);
+  memcpy(&row->chars[row->size], s, len);
+  row->size += len;
+  row->chars[row->size] = '\0';
+  editorUpdateRow(row);
+  E.dirty++;
+}
 
-// void editorRowDelChar(erow *row, int at) {
-//   if (at < 0 || at >= row->size) return;
-//   memmove(&row->chars[at], &row->chars[at + 1], row->size - at);
-//   row->size--;
-//   editorUpdateRow(row);
-//   E.dirty++;
-// }
+void editorRowDelChar(erow *row, int at) {
+  if (at < 0 || at >= row->size)
+    return;
+  memmove(&row->chars[at], &row->chars[at + 1], row->size - at);
+  row->size--;
+  editorUpdateRow(row);
+  E.dirty++;
+}
 
 // /*** editor operations ***/
 
@@ -905,26 +909,48 @@ void editorSelectSyntaxHighlight() {
 
 // /*** init ***/
 
-// void initEditor() {
-//   E.cx = 0;
-//   E.cy = 0;
-//   E.rx = 0;
-//   E.rowoff = 0;
-//   E.coloff = 0;
-//   E.numrows = 0;
-//   E.row = NULL;
-//   E.dirty = 0;
-//   E.filename = NULL;
-//   E.statusmsg[0] = '\0';
-//   E.statusmsg_time = 0;
-//   E.syntax = NULL;
-
-//   if (getWindowSize(&E.screenrows, &E.screencols) == -1)
-//     die("getWindowSize");
-//   E.screenrows -= 2;
-// }
+void initEditor() {
+  E.cx = 0;
+  E.cy = 0;
+  E.rx = 0;
+  E.rowoff = 0;
+  E.coloff = 0;
+  E.numrows = 0;
+  E.row = nullptr;
+  E.dirty = 0;
+  E.filename = nullptr;
+  E.statusmsg[0] = '\0';
+  E.statusmsg_time = 0;
+  E.syntax = nullptr;
+  std::pair<std::size_t, std::size_t> size = Term::get_size();
+  E.screenrows = size.first;
+  E.screencols = size.second;
+  E.screenrows -= 2;
+}
 
 int main(int argc, char *argv[]) {
+  // We must put all code in try/catch block, otherwise destructors are not
+  // being called when exception happens and the terminal is not put into
+  // correct state.
+  try {
+    // check if the terminal is capable of handling input
+    if (!Term::is_stdin_a_tty()) {
+      std::cout << "The terminal is not attached to a TTY and therefore can't "
+                   "catch user input. Existing...\n";
+      return 1;
+    }
+    Term::Terminal term(true, true, false);
+    initEditor();
+    if (argc >= 2) {
+    }
+  } catch (const Term::Exception &re) {
+    std::cerr << "cpp-terminal error: " << re.what() << std::endl;
+    return 2;
+  } catch (...) {
+    std::cerr << "Unknown error." << std::endl;
+    return 1;
+  }
+  return 0;
   // enableRawMode();
   // initEditor();
   // if (argc >= 2) {
